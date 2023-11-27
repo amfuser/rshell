ab8b4ab36efb7facb6e15b0b0b6b4c61799d3fc4
Aaron Franke <aaron.franke13@gmail.com>
2023-11-26 20:38:08 -0700

    Checkin latest chnages to the shell.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 fn main() {
     use std::io::{stdin,stdout,Write};
     let mut s=String::new();
@@ -11,12 +13,23 @@ fn main() {
         if let Some('\r')=s.chars().next_back() {
             s.pop();
         }
+        if s.eq_ignore_ascii_case("ls") {
+           dir();
+        }
         if s.eq_ignore_ascii_case("exit") {
             break;
         }
         else {
-            println!("You typed: {}",s);
+            //println!("You typed: {}",s);
             s = String::new();
         }
+    }
+}
+
+fn dir() {
+    let paths = fs::read_dir("./").unwrap();
+    // TODO: Fix the println to print the appropriate directory info 
+    for path in paths {
+        println!("Name: {}", path.unwrap().path().display())
     }
 }
